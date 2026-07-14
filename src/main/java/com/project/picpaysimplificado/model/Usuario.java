2589a5ef657f91bdff4ca6e90605a93e8e6b26e9
Pedro Henrique <pedrohenrique096k@gmail.com>
2026-07-13 21:29:11 -0300

    feat: Adicionado a coluna valor na entidade usuario e criando um metodo na service para buscar um usuario

--- src/main/java/com/project/picpaysimplificado/model/Usuario.java
+++ src/main/java/com/project/picpaysimplificado/model/Usuario.java
@@ -7,6 +7,8 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.math.BigDecimal;
+
 @Entity(name = "usuario")
 @Table(name = "usuario")
 @Getter
@@ -28,6 +30,8 @@ public class Usuario {
 
     @Enumerated(EnumType.STRING)
     private TipoUsuario tipoUsuario;
+
+    private BigDecimal valor;
 
     public Usuario(UsuarioRequest usuarioRequest) {
         this.nome = usuarioRequest.nome();
--- src/main/java/com/project/picpaysimplificado/services/UsuarioService.java
+++ src/main/java/com/project/picpaysimplificado/services/UsuarioService.java
@@ -6,6 +6,8 @@ import com.project.picpaysimplificado.model.Usuario;
 import com.project.picpaysimplificado.repository.UsuarioRepository;
 import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
+
 @Service
 public class UsuarioService {
     private final UsuarioRepository usuarioRepository;
@@ -16,6 +18,7 @@ public class UsuarioService {
 
     public UsuarioResponse salvarUsuario(UsuarioRequest usuarioRequest) {
         Usuario usuario = new Usuario(usuarioRequest);
+        usuario.setValor(new BigDecimal(100));
         Usuario usuarioSalvo = usuarioRepository.save(usuario);
         return new UsuarioResponse(usuarioSalvo.getId(), usuarioSalvo.getNome(), usuarioSalvo.getTipoUsuario());
     }
@@ -23,5 +26,9 @@ public class UsuarioService {
     public UsuarioResponse getUsuarioById(Long id) {
         Usuario usuario = usuarioRepository.findById(id).orElseThrow(() -> new RuntimeException("Usuário não encontrado"));
         return new UsuarioResponse(usuario.getId(), usuario.getNome(), usuario.getTipoUsuario());
+    }
+
+    public Usuario buscarUsuario(Long id) {
+        return usuarioRepository.findById(id).orElseThrow(() -> new RuntimeException("Usuário não encontrado"));
     }
 }
