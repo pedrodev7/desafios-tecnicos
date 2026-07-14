6c3b037600646ca452d87a74f790eeb90f58be2d
Pedro Henrique <pedrohenrique096k@gmail.com>
2026-07-13 23:47:51 -0300

    commit inicial

---
+++ ms-util/ms-authorize/src/main/java/com/project/ms_authorize/MsAuthorizeApplication.java
@@ -0,0 +1,13 @@
+package com.project.ms_authorize;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsAuthorizeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsAuthorizeApplication.class, args);
+	}
+
+}
---
+++ ms-util/ms-authorize/src/test/java/com/project/ms_authorize/MsAuthorizeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.ms_authorize;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsAuthorizeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
